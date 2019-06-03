637a5f8ccf82767d48bd48303053acfc911d8ee7
peterSzalma <petiszalma@gmail.com>
2019-06-03 17:53:09 +0200

    add buttons to front and back-end

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -8,10 +8,38 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0, btn_Add, btn_Sub,
+            btn_Mul, btn_Div, btn_calc, btn_dec, btn_clear;
+
+    EditText ed1;
+
+    float value1, value2;
+
+    boolean mAddition, mSubtract, mMultiplication, mDivision;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_0 = (Button) findViewById(R.id.btn_0);
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_5 = (Button) findViewById(R.id.btn_5);
+        btn_6 = (Button) findViewById(R.id.btn_6);
+        btn_7 = (Button) findViewById(R.id.btn_7);
+        btn_8 = (Button) findViewById(R.id.btn_8);
+        btn_9 = (Button) findViewById(R.id.btn_9);
+        btn_Add = (Button) findViewById(R.id.btn_Add);
+        btn_Div = (Button) findViewById(R.id.btn_Div);
+        btn_Sub = (Button) findViewById(R.id.btn_Sub);
+        btn_Mul = (Button) findViewById(R.id.btn_Mul);
+        btn_calc = (Button) findViewById(R.id.btn_calc);
+        btn_dec = (Button) findViewById(R.id.btn_dec);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        ed1 = (EditText) findViewById(R.id.edText1);
 
     }
 }
